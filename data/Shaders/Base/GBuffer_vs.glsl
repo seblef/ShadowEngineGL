72b5cabba89f8a493fa650cd5974b1364aaabda3
seb <sebast.lefort@gmail.com>
2021-10-07 14:36:49 +0200

    Fixed normal mapping

--- data/Shaders/Base/GBuffer_vs.glsl
+++ data/Shaders/Base/GBuffer_vs.glsl
@@ -24,13 +24,10 @@ void main(void)
 	v_texBase=uv;
 
 	t=normalize(rot * tangent);
-	b=normalize(rot * binormal);
+	t = normalize(t - dot(t, v_normal) * v_normal);
+	b = cross(v_normal, t);
 
 	v_tangent=mat3(t,b,v_normal);
-/*	v_tangent[0]=vec3(t.x, b.x, v_normal.x);
-	v_tangent[1]=vec3(t.y, b.y, v_normal.y);
-	v_tangent[2]=vec3(t.z, b.z, v_normal.z);
-*/
 	gl_Position=v_worldPos;
 }
 
